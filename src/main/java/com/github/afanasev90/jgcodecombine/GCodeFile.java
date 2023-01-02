be5541483c758c3274a1b96b0ba035f572b65585
afanasev90 <web.afanasyev@gmail.com>
2023-01-02 16:41:19 +0700

    First

---
+++ src/main/java/com/github/afanasev90/jgcodecombine/GCodeFile.java
@@ -0,0 +1,7 @@
+package com.github.afanasev90.jgcodecombine;
+
+public class GCodeFile {
+    static boolean statictest(){
+        return true;
+    }
+}
---
+++ src/test/java/com/github/afanasev90/jgcodecombine/GCodeFileTest.java
@@ -0,0 +1,13 @@
+package com.github.afanasev90.jgcodecombine;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GCodeFileTest {
+
+    @Test
+    void statictest() {
+        assertTrue(GCodeFile.statictest());
+    }
+}
